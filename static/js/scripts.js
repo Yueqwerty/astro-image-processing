72d45835638b4a729125a56d72e84f468b09e2b5
Yue <83837742+Yueqwerty@users.noreply.github.com>
2024-06-01 17:07:32 -0400

    fixes

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -13,7 +13,7 @@ document.getElementById('uploadForm').addEventListener('submit', function(event)
         var resultDiv = document.getElementById('result');
         resultDiv.innerHTML = `
             <p>Original Image: <a href="/data/images/${data.filename}" target="_blank">View</a></p>
-            <p>Processed Image: <a href="/data/processed_images/${data.result.processed.split('/').pop()}" target="_blank">View</a></p>
+            <p>Processed Image: <a href="/data/${data.result.processed.split('/').pop()}" target="_blank">View</a></p>
             <p>Galaxy Type: ${data.galaxy_type}</p>
         `;
     })
